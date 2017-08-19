056d9483bb1b4cc65099db8dade994db44e343ff
kenmatsumoto <ken@nri.com>
2017-08-18 17:13:59 -0700

    delete db_insert
    You have to install & setup database table & data with *.sql on sqlite3 db

--- RESTsampledb/apppost.py
+++ RESTsampledb/apppost.py
@@ -21,14 +21,6 @@ app.config.from_envvar('APPPOST_SETTINGS', silent=True)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-''' def init_db():
-    db = g.db
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-    db.close()
-'''
-
 @app.before_request
 def before_request():
     g.db = connect_db()
